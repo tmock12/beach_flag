469a1e7ed49b00ded31a8411940c5844c626f826
Taylor Mock <tmock12@gmail.com>
2021-04-16 17:49:45 -0500

    Mix new

---
+++ lib/beach_flag_text_parser.ex
@@ -0,0 +1,18 @@
+defmodule BeachFlagTextParser do
+  @moduledoc """
+  Documentation for `BeachFlagTextParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BeachFlagTextParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/beach_flag_text_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule BeachFlagTextParserTest do
+  use ExUnit.Case
+  doctest BeachFlagTextParser
+
+  test "greets the world" do
+    assert BeachFlagTextParser.hello() == :world
+  end
+end
